7e0d8b43e217a77b94b126a93e7ed786386db5c6
ssu57 <132045002+ssu57@users.noreply.github.com>
2024-01-03 21:30:31 +0900

    1을 제외한 소수

--- 1213.c
+++ 1213.c
@@ -1,31 +1,10 @@
 #include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-
-int main()
-{
-	int i, j, k = 0;
-	int t_arr[4][5] = { 0, };
-	int max=0, min=101;
-
-	srand(time(NULL));
-	for (i = 0; i < 4; i++) {
-		for (k = 0; k <= 2; k++)
-			t_arr[i][k] += 1 + rand() % 100;
+int main() {
+	int i, j;
+	for (i = 2; i <= 100; i++) {
+		for (j = 2; i % j != 0; j++);
+		if (i == j)
+			printf("%d\n", i);
 	}
-	for (i = 0; i < 4; i++) {
-		for (j = 0; j < 3; j++)
-			t_arr[i][3] += t_arr[i][j];
-	}
-	for (i = 0; i < 4; i++) {
-		t_arr[i][4] = t_arr[i][3] / 3;
-	}
-	for (i = 0; i < 4; i++) {
-		if (max < t_arr[i][4])
-			max = t_arr[i][4];
-		if (min > t_arr[i][4])
-			min = t_arr[i][4];
-	}
-	printf("최대값=%d", max);
-	printf("최솟값=%d", min);
-}
+	return 0;
+}//소수 구하기
